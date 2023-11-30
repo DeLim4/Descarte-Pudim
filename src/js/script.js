de6ae6dc6e8d442b1f723dd61079fb626f32d42e
Gustavo <123416253+DeLim4@users.noreply.github.com>
2023-11-30 15:20:06 -0300

    Update script.js
    table

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
   // Lista para armazenar os descartados
   var produtosDescartados = [];
 
-  //  tipos de produtos 
+  // Tipos de produtos
   var tiposDeProdutos = {
       pudins125g: [
           "Pudim Leite moça - Indivudual",
@@ -41,49 +41,34 @@
           "Pepsi Black",
       ],
       bolos: [
-        "Chocolate c/ Brigadeiro",
-        "Cenoura c/ Brigadeiro",
-        "Casadinho",
-        "Red velvet",
-        "Prestigio",
-        "Surpresa de Uva",
+          "Chocolate c/ Brigadeiro",
+          "Cenoura c/ Brigadeiro",
+          "Casadinho",
+          "Red velvet",
+          "Prestigio",
+          "Surpresa de Uva",
       ],
       brownie: [
-        "Brownie c/ brigadeiro",
-        "Brownie c ninho e nutella",
-        
+          "Brownie c/ brigadeiro",
+          "Brownie c ninho e nutella",
+
       ],
       brigadeiro: [
-        "Tradicional",
+          "Tradicional",
           "Dark",
           "Casadinho",
           "Pistache",
-        "Ninho c/ nutella"
-        
+          "Ninho c/ nutella"
+
       ],
       coxinha: [
-        "Coxinha de Pistache",
+          "Coxinha de Pistache",
           "Bombom de uva",
           "Brigadereiro no Belga",
           "Belga no belga",
-        
-    ],
-    boloCaseiro:[
-      "Laranja",
-      "Cenoura c/ chocolate",
-      "Chocolate c/ chocolate",
-      "Fubá c/ goiabada",
-      "Fubá",
-      "Milho",
-"RedVelvet",
 
-    ],
-Caldas:[
-  "Chocolate",
-  "Goiabada",
-  "Chocolate Branco",
-],
-   
+      ],
+
   };
 
   // Adicionar um ouvinte de eventos para o tipo de produto
@@ -93,111 +78,146 @@ Caldas:[
   function atualizarProdutos() {
       var tipoSelecionado = document.getElementById("selecionarTipoProduto").value;
       var selectProduto = document.getElementById("selecionarProduto");
-      
+
       selectProduto.innerHTML = ""; // Limpar as opções existentes
 
-// Obter a lista de produtos com base no tipo selecionado
-var produtosDoTipo = tiposDeProdutos[tipoSelecionado];
+      // Obter a lista de produtos com base no tipo selecionado
+      var produtosDoTipo = tiposDeProdutos[tipoSelecionado];
 
-// Preencher as opções do produto com base na lista
-produtosDoTipo.forEach(function (produto) {
-var option = document.createElement("option");
-option.value = produto; // Valor e texto são iguais
-option.text = produto;
-selectProduto.appendChild(option);
-});
-}
+      // Preencher as opções do produto com base na lista
+      produtosDoTipo.forEach(function (produto) {
+          var option = document.createElement("option");
+          option.value = produto; // Valor e texto são iguais
+          option.text = produto;
+          selectProduto.appendChild(option);
+      });
+  }
 
-function adicionarProduto() {
-var produtoSelecionado = document.getElementById("selecionarProduto");
-var nomeProduto = produtoSelecionado.options[produtoSelecionado.selectedIndex].text;
-var quantidadeProduto = parseInt(document.getElementById("quantidadeProduto").value, 10);
-var localSelecionado = document.getElementById("selecionarLocal").value;
+  function adicionarProduto() {
+      var produtoSelecionado = document.getElementById("selecionarProduto");
+      var nomeProduto = produtoSelecionado.options[produtoSelecionado.selectedIndex].text;
+      var quantidadeProduto = parseInt(document.getElementById("quantidadeProduto").value, 10);
+      var localSelecionado = document.getElementById("selecionarLocal").value;
 
-// Validar se os campos não estão vazios
-if (isNaN(quantidadeProduto)) {
-alert("Por favor, preencha a quantidade do produto.");
-return;
-}
+      // Validar se os campos não estão vazios
+      if (isNaN(quantidadeProduto)) {
+          alert("Por favor, preencha a quantidade do produto.");
+          return;
+      }
 
-// Verificar se o local já está na lista
-var localExistente = produtosDescartados.find(function (produto) {
-return produto.local === localSelecionado;
-});
+      // Verificar se o local já está na lista
+      var localExistente = produtosDescartados.find(function (produto) {
+          return produto.local === localSelecionado;
+      });
 
-if (localExistente) {
-// Se o local já existe, apenas adicionar o novo produto ou atualizar a quantidade
-var produtoExistente = localExistente.produtos.find(function (p) {
-  return p.nome === nomeProduto;
-});
+      if (localExistente) {
+          // Se o local já existe, apenas adicionar o novo produto ou atualizar a quantidade
+          var produtoExistente = localExistente.produtos.find(function (p) {
+              return p.nome === nomeProduto;
+          });
 
-if (produtoExistente) {
-  // Se o produto já existe, somar a quantidade
-  produtoExistente.quantidade += quantidadeProduto;
-} else {
-  // Se o produto não existe, adicionar à lista de produtos do local
-  localExistente.produtos.push({
-      nome: nomeProduto,
-      quantidade: quantidadeProduto
-  });
-}
-} else {
-// Se o local não existe, adicionar à lista
-var novoLocal = {
-  local: localSelecionado,
-  produtos: [{
-      nome: nomeProduto,
-      quantidade: quantidadeProduto
-  }]
-};
-produtosDescartados.push(novoLocal);
-}
+          if (produtoExistente) {
+              // Se o produto já existe, somar a quantidade
+              produtoExistente.quantidade += quantidadeProduto;
+          } else {
+              // Se o produto não existe, adicionar à lista de produtos do local
+              localExistente.produtos.push({
+                  nome: nomeProduto,
+                  quantidade: quantidadeProduto
+              });
+          }
+      } else {
+          // Se o local não existe, adicionar à lista
+          var novoLocal = {
+              local: localSelecionado,
+              produtos: [{
+                  nome: nomeProduto,
+                  quantidade: quantidadeProduto
+              }]
+          };
+          produtosDescartados.push(novoLocal);
+      }
 
-// Limpar os campos do formulário
-document.getElementById("quantidadeProduto").value = "";
+      // Limpar os campos do formulário
+      document.getElementById("quantidadeProduto").value = "";
 
-// Atualizar a lista de produtos na página
-atualizarListaProdutos();
-}
+      // Atualizar a lista de produtos na página
+      atualizarListaProdutos();
+  }
 
-function atualizarListaProdutos() {
+  function atualizarListaProdutos() {
+      var listaProdutosElement = document.getElementById("listaProdutosUl");
+      var corpoTabelaProdutos = document.getElementById("corpoTabelaProdutos");
+
+      // Limpar a lista antes de atualizar
+      listaProdutosElement.innerHTML = "";
+      corpoTabelaProdutos.innerHTML = "";
+
+      // Adicionar cada local e seus produtos à lista e à tabela
+      produtosDescartados.forEach(function (local) {
+          var listItemLocal = document.createElement("li");
+          listItemLocal.classList.add("local"); // Adicionar a classe para estilizar
+          listItemLocal.textContent = local.local;
+          listaProdutosElement.appendChild(listItemLocal);
+
+          var rowTabelaProdutos = document.createElement("tr");
+          var cellLocal = document.createElement("td");
+          cellLocal.textContent = local.local;
+          rowTabelaProdutos.appendChild(cellLocal);
+
+          local.produtos.forEach(function (produto) {
+              var listItemProduto = document.createElement("li");
+              listItemProduto.textContent = produto.quantidade + " unidade(s) de " + produto.nome;
+              listaProdutosElement.appendChild(listItemProduto);
+
+              var cellProduto = document.createElement("td");
+              cellProduto.textContent = produto.nome;
+
+              var cellQuantidade = document.createElement("td");
+              cellQuantidade.textContent = produto.quantidade;
+
+              var rowTabelaProduto = document.createElement("tr");
+              rowTabelaProduto.appendChild(cellLocal.cloneNode(true));
+              rowTabelaProduto.appendChild(cellProduto);
+              rowTabelaProduto.appendChild(cellQuantidade);
+              corpoTabelaProdutos.appendChild(rowTabelaProduto);
+          });
+      });
+  }
+  function alternarVisualizacao() {
 var listaProdutosElement = document.getElementById("listaProdutos");
+var tabelaProdutosElement = document.getElementById("tabelaProdutos");
 
-// Limpar a lista antes de atualizar
-listaProdutosElement.innerHTML = "";
-
-// Adicionar cada local e seus produtos à lista
-produtosDescartados.forEach(function (local) {
-var listItemLocal = document.createElement("li");
-listItemLocal.classList.add("local"); // Adicionar a classe para estilizar
-listItemLocal.textContent = local.local;
-listaProdutosElement.appendChild(listItemLocal);
-
-local.produtos.forEach(function (produto) {
-  var listItemProduto = document.createElement("li");
-  listItemProduto.textContent = produto.quantidade + " unidade(s) de " + produto.nome;
-  listaProdutosElement.appendChild(listItemProduto);
-});
-});
+if (listaProdutosElement.style.display !== "none") {
+  // Se a lista está visível, esconde a lista e exibe a tabela
+  listaProdutosElement.style.display = "none";
+  tabelaProdutosElement.style.display = "block";
+} else {
+  // Se a tabela está visível, esconde a tabela e exibe a lista
+  listaProdutosElement.style.display = "block";
+  tabelaProdutosElement.style.display = "none";
+}
 }
 
-function copiarParaWhatsApp() {
-var textoParaCopiar = "Lista de Produtos Descartados:\n";
 
-produtosDescartados.forEach(function (local) {
-textoParaCopiar += `${local.local}:\n`;
+  function copiarParaWhatsApp() {
+      var textoParaCopiar = "Lista de Produtos Descartados:\n";
 
-local.produtos.forEach(function (produto) {
-  textoParaCopiar += `  - ${produto.quantidade} unidade(s) de ${produto.nome}\n`;
-});
-});
+      produtosDescartados.forEach(function (local) {
+          textoParaCopiar += `${local.local}:\n`;
 
-// Copiar para a área de transferência
-navigator.clipboard.writeText(textoParaCopiar)
-.then(function () {
-  alert("Lista copiada para a área de transferência. Cole no WhatsApp.");
-})
-.catch(function (err) {
-  console.error('Erro ao copiar para a área de transferência:', err);
-});
+          local.produtos.forEach(function (produto) {
+              textoParaCopiar += `  - ${produto.quantidade} unidade(s) de ${produto.nome}\n`;
+          });
+      });
+
+      // Copiar para a área de transferência
+      navigator.clipboard.writeText(textoParaCopiar)
+          .then(function () {
+              alert("Lista copiada para a área de transferência. Cole no WhatsApp.");
+          })
+          .catch(function (err) {
+              console.error('Erro ao copiar para a área de transferência:', err);
+          });
+
 }
